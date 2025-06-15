1fc382edb93a67e822cf1c88026e58575acc7409
Greve <198575622+EnsureStudioGreve@users.noreply.github.com>
2025-06-15 07:52:34 +0200

    Auto commit

--- web/src/components/inventory/InventorySlot.tsx
+++ web/src/components/inventory/InventorySlot.tsx
@@ -82,7 +82,7 @@ const InventorySlot: React.ForwardRefRenderFunction<HTMLDivElement, SlotProps> =
             onDrop(source, { inventory: inventoryType, item: { slot: item.slot } });
             break;
         }
-           DragAudio();
+          DragAudio();
       },
       canDrop: (source) =>
         (source.item.slot !== item.slot || source.inventory !== inventoryType) &&
